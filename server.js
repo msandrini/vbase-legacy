e71d2e10b91a7fe94bb69c35a8a96f7bda285360
Marcos Sandrini <marcos.sandrini@crosslend.com>
2017-01-13 15:36:56 +0100

    port to work online

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require('express');
 
 const app = express();
 
+app.set('port', (process.env.PORT || 5000));
+
 app.get('/', (req, res) => {
     res.json({ success: true });
 });
 
-app.listen(5000, () => {
-    console.log('Node app is running on port 5000');
+app.listen(app.get('port'), () => {
+    console.log('Node app is running on port ' + app.get('port'));
 });
