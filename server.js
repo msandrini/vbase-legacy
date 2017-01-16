b643de8a1b63a241fd36642c62cd6d92d559b8fd
Marcos Sandrini <marcos.sandrini@crosslend.com>
2017-01-16 12:57:33 +0100

    implemented mongo

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const mongo = require('mongodb').MongoClient;
+const mongoUrl = 'mongodb://localhost:27017/myproject';
 
 const app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/', (req, res) => {
-    res.json({ success: true });
+    mongo.connect(mongoUrl, (err, db) => {
+    	const infos = db.collection('infos');
+    	infos.find({}).toArray((err, docs) => {
+	    	res.json({ thereIsDb: true, docs: docs });
+	    	db.close();
+    	});
+    });
 });
 
 app.listen(app.get('port'), () => {
