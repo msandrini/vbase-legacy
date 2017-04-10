e3e13920d13f778fc04e9efb677b63553b509abd
Marcos Sandrini <marcos.sandrini@crosslend.com>
2017-04-10 11:45:52 +0200

    test routine for db

--- server/ops.server.js
+++ server/ops.server.js
@@ -1,14 +1,9 @@
 const ops = (db, payload) => {
-    return new Promise((resolve, reject) => {
-        db.collection('series').find({ 'text.pt-br': { $ne: '' } }).toArray().then((results, error) => {
-            if (error) {
-                reject(error)
-            } else {
-                const map = results.map(r => r._id + `: ` + r.text['pt-br'])
-                resolve({ feedback: map })
-            }
-        })
-    })
+	return new Promise((resolve, reject) => {
+		db.listCollections().toArray().then((results, error) => {
+			resolve(results, error)
+		})
+	})
 }
 
 module.exports = ops
