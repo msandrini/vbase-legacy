aa182f9391dcf504f17a8a0bf1bbf1f076996553
Marcos Sandrini <msandrinidesign@gmail.com>
2018-04-19 14:24:53 -0300

    dotenv installed, now only for tests

--- test.js
+++ test.js
@@ -1,6 +1,9 @@
 
 const mongo = require('mongodb').MongoClient
 const logger = require('mongodb').Logger
+const dotenv = require('dotenv')
+
+dotenv.load()
 
 const url = process.env.CONNECTION
 mongo.connect(url, null, (connectionError, db) => {
